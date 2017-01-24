c45defc605bb0f09a0d4e61f8853dce27853d5c2
Kamil Dziuba <kamil.dziuba@gmail.com>
2017-01-24 21:08:19 +0100

    commenttask

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// plik scripts.js
+
+var name = prompt('Jak się nazywasz?');
+// alert('Twoje imię to: ' + name);
+console.log('Witaj, ' + name);
+
+// komentarz liniowy
+
+/*
+
+komentarz blokowy
+
+mentor będzie happy ;)
+
+*/
